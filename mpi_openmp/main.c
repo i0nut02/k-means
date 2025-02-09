d8784bba59ce48f86624ad3593d8748253f7bb09
I0nut02 <ionu.radu02@gmail.com>
2025-02-09 17:17:42 +0100

    refactor

--- mpi_openmp/main.c
+++ mpi_openmp/main.c
@@ -9,9 +9,263 @@
 #include <mpi.h>
 #include <omp.h>
 
-#include "../include/const.h"
-#include "../include/kmeans.h"
-#include "../include/file.h"
+#define MAXLINE 200000
+#define DEFAULT_CLASS -1
+#define SEED 0
+
+// Error codes
+#define INPUT_ERR -1
+#define MEMORY_ALLOCATION_ERR -2
+#define TOO_MUCH_COLUMNS_ERR -3
+#define READ_ERR -4
+#define WRITE_ERR -5
+
+#define CACHE_LINE_SIZE 64
+#define PAD_INT (CACHE_LINE_SIZE / sizeof(int))
+#define PAD_FLOAT (CACHE_LINE_SIZE / sizeof(int))
+
+void elementIntArray(int *array, int value, int size) {
+    for(int i = 0; i < size; i++) {
+        array[i] = value;
+    }
+}
+
+void elementFloatArray(float *array, float value, int size) {
+    for(int i = 0; i < size; i++) {
+        array[i] = value;
+    }
+}
+
+void initCentroids(const float* data, float* centroids, const int K, const int n, const int dim) {
+    int* dataPointAssigned = (int*) calloc(n, sizeof(int));
+
+    if (dataPointAssigned == NULL) {
+        exit(MEMORY_ALLOCATION_ERR);
+    }
+
+    srand(SEED);
+    for (int i = 0; i < K; i++) {
+        int idx = rand() % n;
+        while (dataPointAssigned[idx] != 0) {
+            idx = (idx + 1) % n;
+        }
+        memcpy(&centroids[i*dim], &data[idx*dim], dim * sizeof(float));
+        dataPointAssigned[idx] = -1;
+    }
+    
+    free(dataPointAssigned);
+    return;
+}
+
+// Get local data range for each MPI process
+void getLocalRange(int rank, int size, int totalPoints, int *start, int *count) {
+    int quotient = totalPoints / size;
+    int remainder = totalPoints % size;
+    *start = quotient * rank + (rank < remainder ? rank : remainder);
+    *count = quotient + (rank < remainder ? 1 : 0);
+}
+
+// OpenMP
+void assignDataToCentroids(const float *data, const float *centroids, int *classMap, 
+    int numPoints, int dimPoints, int K, int *changes, int numThreads) {
+    int localChanges = 0;
+
+    #pragma omp parallel for reduction(+:localChanges) num_threads(numThreads)
+    for (int i = 0; i < numPoints; i++) {
+        float minDist = FLT_MAX;
+        int newClass = -1;
+
+        for (int k = 0; k < K; k++) {
+            int index = k % K;
+            float dist = 0.0f;
+
+            for (int d = 0; d < dimPoints; d++) {
+                float diff = data[i * dimPoints + d] - centroids[index * dimPoints + d];
+                dist = fmaf(diff, diff, dist);
+            }
+
+            if (dist < minDist) {
+                minDist = dist;
+                newClass = index;
+            }
+        }
+
+        if (classMap[i] != newClass) {
+            classMap[i] = newClass;
+            localChanges++;
+        }
+    }
+
+    *changes = localChanges;
+}
+
+
+void updateLocalVariables(const float *data, float *auxCentroids, const int *classMap, 
+    int *pointsPerClass, int numPoints, int dimPoints, int K) {
+
+    #pragma omp parallel
+    {
+        int* localPointsPerClass = (int*) calloc(K, sizeof(int));
+        float* localAuxCentroids = (float*) calloc(K * dimPoints, sizeof(float));
+
+		#pragma omp for
+        for(int i = 0; i < numPoints; i++) {
+            int class = classMap[i];
+            localPointsPerClass[class] += 1;
+
+            for(int j = 0; j < dimPoints; j++) {
+                localAuxCentroids[class * dimPoints + j] += data[i * dimPoints + j];
+            }
+        }
+
+		#pragma omp critical
+        {
+            for (int k = 0; k < K; k++) {
+                pointsPerClass[k] += localPointsPerClass[k];
+                for (int j = 0; j < dimPoints; j++)
+                {
+                    auxCentroids[k * dimPoints + j] += localAuxCentroids[k * dimPoints + j];
+                }
+            }
+        }
+        free(localPointsPerClass);
+        free(localAuxCentroids);
+    }
+ }
+
+float updateCentroids(float *centroids, float *auxCentroids, 
+    const int *pointsPerClass, int dimPoints, int K) {
+
+    #pragma omp for
+    for(int k = 0; k < K; k++) {
+        int pointsInClass = pointsPerClass[k];
+        for(int j = 0; j < dimPoints; j++){
+            auxCentroids[k * dimPoints + j] = auxCentroids[k * dimPoints + j] / pointsInClass;
+        } 
+    }
+
+    float maxDist = 0.0f;
+
+    #pragma omp parallel for reduction(max:maxDist)
+    for(int k = 0; k < K; k++){
+
+        float dist = 0.0f;
+        #pragma omp reduction(+:dist)
+        for (int d = 0; d < dimPoints; d++) {
+            float diff = centroids[k * dimPoints + d] - auxCentroids[k * dimPoints + d];
+            dist = fmaf(diff, diff, dist);
+        }
+
+        if(dist > maxDist) {
+            maxDist = dist;
+        }
+    }
+
+    return maxDist;
+}
+
+
+void showFileError(int error, char* filename) {
+    printf("Error\n");
+    switch (error) {
+        case TOO_MUCH_COLUMNS_ERR:
+            fprintf(stderr,"\tFile %s has too many columns.\n", filename);
+            fprintf(stderr,"\tThe maximum number of columns has been exceeded. MAXLINE: %d.\n", MAXLINE);
+            break;
+        case READ_ERR:
+            fprintf(stderr,"Error reading file: %s.\n", filename);
+            break;
+        case WRITE_ERR:
+            fprintf(stderr,"Error writing file: %s.\n", filename);
+            break;
+    }
+    fflush(stderr);    
+}
+
+int readInput(char* filename, int *lines, int *samples) {
+    FILE *fp;
+    char line[MAXLINE] = "";
+    char *ptr;
+    const char *delim = "\t";
+    int contlines, contsamples = 0;
+    
+    contlines = 0;
+
+    if ((fp=fopen(filename,"r"))!=NULL) {
+        while(fgets(line, MAXLINE, fp)!= NULL) {
+            if (strchr(line, '\n') == NULL) {
+                return -1;
+            }
+            contlines++;       
+            ptr = strtok(line, delim);
+            contsamples = 0;
+            while(ptr != NULL) {
+                contsamples++;
+                ptr = strtok(NULL, delim);
+            }        
+        }
+        fclose(fp);
+        *lines = contlines;
+        *samples = contsamples;  
+        return 0;
+    }
+    else {
+        return READ_ERR;
+    }
+}
+
+int readInput2(char* filename, float* data) {
+    FILE *fp;
+    char line[MAXLINE] = "";
+    char *ptr;
+    const char *delim = "\t";
+    int i = 0;
+    
+    if ((fp=fopen(filename,"rt"))!=NULL) {
+        while(fgets(line, MAXLINE, fp)!= NULL) {         
+            ptr = strtok(line, delim);
+            while(ptr != NULL) {
+                data[i] = atof(ptr);
+                i++;
+                ptr = strtok(NULL, delim);
+            }
+        }
+        fclose(fp);
+        return 0;
+    }
+    else {
+        return READ_ERR;
+    }
+}
+
+int writeResult(int *classMap, int lines, const char* filename) {    
+    FILE *fp;
+    
+    if ((fp=fopen(filename,"wt"))!=NULL) {
+        for(int i=0; i<lines; i++) {
+            fprintf(fp,"%d\n",classMap[i]);
+        }
+        fclose(fp);  
+   
+        return 0;
+    }
+    else {
+        return WRITE_ERR;
+    }
+}
+
+int writeLog(const char* filename, const char* message) {    
+    FILE *fp;
+    
+    if ((fp=fopen(filename,"wt"))!=NULL) {
+        fprintf(fp, "%s", message);
+        fclose(fp);   
+        return 0;
+    }
+    else {
+        return WRITE_ERR;
+    }
+}
 
 
 int main(int argc, char* argv[]) {
