ddcb78bd02bbaf4b72794baa606b7ccf7edc594b
I0nut02 <ionu.radu02@gmail.com>
2025-02-09 10:19:13 +0100

    solve issue

--- mpi_openmp/src/kmeans.c
+++ mpi_openmp/src/kmeans.c
@@ -1,3 +1,50 @@
+#include "../include/kmeans.h"
+
+
+void elementIntArray(int *array, int value, int size) {
+    #pragma omp parallel for
+    for(int i = 0; i < size; i++) {
+        array[i] = value;
+    }
+}
+
+void elementFloatArray(float *array, float value, int size) {
+    #pragma omp parallel for
+    for(int i = 0; i < size; i++) {
+        array[i] = value;
+    }
+}
+
+void initCentroids(const float* data, float* centroids, const int K, const int n, const int dim) {
+    int* dataPointAssigned = (int*) calloc(n, sizeof(int));
+
+    if (dataPointAssigned == NULL) {
+        exit(MEMORY_ALLOCATION_ERR);
+    }
+
+    srand(SEED);
+    for (int i = 0; i < K; i++) {
+        int idx = rand() % n;
+        while (dataPointAssigned[idx] != 0) {
+            idx = (idx + 1) % n;
+        }
+        memcpy(&centroids[i*dim], &data[idx*dim], dim * sizeof(float));
+        dataPointAssigned[idx] = -1;
+    }
+    
+    free(dataPointAssigned);
+    return;
+}
+
+// Get local data range for each MPI process
+void getLocalRange(int rank, int size, int totalPoints, int *start, int *count) {
+    int quotient = totalPoints / size;
+    int remainder = totalPoints % size;
+    *start = quotient * rank + (rank < remainder ? rank : remainder);
+    *count = quotient + (rank < remainder ? 1 : 0);
+}
+
+// OpenMP
 void assignDataToCentroids(const float *data, const float *centroids, int *classMap, 
     int numPoints, int dimPoints, int K, int *changes) {
 int localChanges = 0;
